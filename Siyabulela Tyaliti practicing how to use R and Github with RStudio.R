b803426408635185535d957a5ae81b33f600f4c3
Siyabulela-Tyaliti <160180219+Siyabulela-Tyaliti@users.noreply.github.com>
2024-09-11 19:57:14 -0400

    Add files via upload
    This is just my first practice R file I uploaded onto my Github account.

---
+++ Siyabulela Tyaliti practicing how to use R and Github with RStudio.R
@@ -0,0 +1,7 @@
+#This is just an R code that I am writing as a means to see what R code from RStudio looks like in Github.
+
+#I will upload this onto my Github profile just to see what R code looks like in Github. 
+
+#This is just practice for me to learn how to upload R scripts to my Github portfolio. 
+
+
